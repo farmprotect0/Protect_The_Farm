01105d88eb25119e193ceef44883ee145a4ffc4e
ho991127 <ho991127@naver.com>
2025-07-28 16:14:16 +0900

    Add|아이템 매니저, 스코어 매니저 추가

---
+++ GOTO_Game/inc/component/ScoreManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <ScriptBehaviour.h>
+#include <InputManager.h>
+#include <RenderManager.h>
+#include <SpriteRenderer.h>
+
+namespace GOTOEngine
+{
+	class ScoreManager : public ScriptBehaviour
+	{
+	public:
+    ScoreManager()
+    {
+
+    }
+		int P1Score = 0;
+		int P2Score = 0;
+		int P1Bonus = 1;
+		int P2Bonus = 1;
+	};
+}
