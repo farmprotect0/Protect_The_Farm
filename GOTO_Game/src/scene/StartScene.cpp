43fa8d78249d1dc16e08335b4151ee55ffd7ddf6
minkee009 <minkee5004@gmail.com>
2025-07-29 21:22:10 +0900

    뒷배경 추가

--- GOTO_Game/src/scene/StartScene.cpp
+++ GOTO_Game/src/scene/StartScene.cpp
@@ -1,5 +1,6 @@
 #include "StartScene.h"
 #include "CrosshairPrefab.h"
+#include "StartMenuPrefab.h"
 #include <Camera.h>
 #include <SpriteRenderer.h>
 #include <Canvas.h>
@@ -9,28 +10,21 @@
 
 void StartScene::Initialize()
 {
+	//카메라
 	auto mainCam = Camera::CreateMainCamera();
 
-	//카메라
-	auto CrossHair1GO = CrosshairPrefab::CreateCrosshair(0);
-	auto CrossHair2GO = CrosshairPrefab::CreateCrosshair(1);
+	if (!GameObject::Find(L"Player"))
+	{
+		auto CrossHair1GO = CrosshairPrefab::CreateCrosshair(0);
+		auto CrossHair2GO = CrosshairPrefab::CreateCrosshair(1);
 
-	CrossHair1GO->GetComponent<SpriteRenderer>()->SetRenderLayer(1 << 1);
-	CrossHair2GO->GetComponent<SpriteRenderer>()->SetRenderLayer(1 << 2);
+		CrossHair1GO->GetComponent<SpriteRenderer>()->SetRenderLayer(1 << 1);
+		CrossHair2GO->GetComponent<SpriteRenderer>()->SetRenderLayer(1 << 2);
 
-	auto canvasGO = new GameObject(L"Canvas");
-	auto canvas = canvasGO->AddComponent<Canvas>();
+		//커서 유지
+		Object::DontDestroyOnLoad(CrossHair1GO);
+		Object::DontDestroyOnLoad(CrossHair2GO);
+	}
 
-	auto TextGO = new GameObject(L"Text");
-	auto textComp = TextGO->AddComponent<Text>();
-
-	textComp->SetFont(L"../Resources/Maplestory Light.ttf");
-	textComp->text = L"Press Enter to Start";
-
-	textComp->GetTransform()->SetParent(canvasGO->GetTransform());	
-	textComp->GetRectTransform()->SetAnchoredPosition({ Screen::GetWidth() * 0.2f, Screen::GetHeight() * 0.2f });
-
-	//커서 유지
-	Object::DontDestroyOnLoad(CrossHair1GO);
-	Object::DontDestroyOnLoad(CrossHair2GO);
+	SCENE_CHANGE_SCENE(L"PlayScene");
 }
