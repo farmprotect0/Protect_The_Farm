b8746939ab1ea6ea5c9c932ac98ebca6e54b4f21
ho991127 <ho991127@naver.com>
2025-07-31 16:19:51 +0900

    사운드매니저수정, 튜토리얼 이미지 컴포넌트 제작

--- GOTO_Game/src/component/SoundManager.cpp
+++ GOTO_Game/src/component/SoundManager.cpp
@@ -12,7 +12,7 @@ void SoundManager::Awake() {
 	{
 		Destroy(GetGameObject());
 	}
-	
+	//경로 지정 및 리스트에 추가
 	
 }
 
@@ -21,9 +21,25 @@ void SoundManager::OnDestroy() {
 		instance = nullptr;
 }
 
+float SoundManager::GetsfxVolume() {
+	return sfxVolume;
+}
+
+float SoundManager::GetbgmVolume() {
+	return bgmVolume;
+}
+
 void SoundManager::SetSFXVolume(float volume) {
 	sfxVolume = volume;
 }
 void SoundManager::SetBGMVolume(float volume) {
 	bgmVolume = volume;
 }
+
+void SoundManager::PlaySFX(const std::string& key) {
+	//sfx볼륨크기로 key에 따라 sfx 재생
+};
+
+void SoundManager::PlayBGM(const std::string& key) {
+	//bgm볼륨크기로 key에 따라 bgm 재생
+};
